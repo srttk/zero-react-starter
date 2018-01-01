9d6df90465764b84ff34ced26a5b0f029e3e0961
Sarath <sarath.hacks@gmail.com>
2018-01-01 22:38:30 +0530

    :fire: First kick :boom:

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.css';
+
+const App = () => (
+
+    <h1>Hello World</h1>
+)
+
+render(<App/>, document.getElementById('app'))
