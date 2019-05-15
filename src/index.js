656313a6e86482ca39b5b5d69611c6daa19a8a76
Sarath <sarath.hacks@gmail.com>
2019-05-15 11:35:59 +0530

    Update babel 7 :rocket: babel class properties plugin (Now support class property as function expression and static functions)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { version } from "../package.json";
 
-import './index.css';
+import "./index.css";
 
-const App = () => (
-    <div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
         <h2>😝 Zero Configuration</h2>
         <h2>React Starter Kit 🔥</h2>
         <div className="description">
-            <p>A Super Simple Reactjs Starter Kit for Humans 👴</p>
-            <p>
-                <a className="github-button" href="https://github.com/saratonite/zero-react-starter" data-icon="octicon-star" aria-label="Star saratonite/zero-react-starter on GitHub">Star</a>
-                &nbsp;
-                <a className="github-button" href="https://github.com/saratonite/zero-react-starter/fork" data-icon="octicon-repo-forked" aria-label="Fork saratonite/zero-react-starter on GitHub">Fork</a>
-            </p>
-            <p className="author">Designed and built with all the love in the world by <a target="_blank" href="https://github.com/saratonite">Sarath ⚡</a></p>
+          <p>A Super Simple Reactjs Starter Kit for Humans 👴</p>
+          <p>Version {version}</p>
+          <p>
+            <a
+              className="github-button"
+              href="https://github.com/saratonite/zero-react-starter"
+              data-icon="octicon-star"
+              aria-label="Star saratonite/zero-react-starter on GitHub"
+            >
+              Star
+            </a>
+            &nbsp;
+            <a
+              className="github-button"
+              href="https://github.com/saratonite/zero-react-starter/fork"
+              data-icon="octicon-repo-forked"
+              aria-label="Fork saratonite/zero-react-starter on GitHub"
+            >
+              Fork
+            </a>
+          </p>
+          <p className="author">
+            Designed and built with all the love in the world by{" "}
+            <a target="_blank" href="https://github.com/saratonite">
+              Sarath ⚡
+            </a>
+          </p>
         </div>
-    </div>
-    
-)
+      </div>
+    );
+  }
+}
 
-render(<App/>, document.getElementById('app'))
+render(<App />, document.getElementById("app"));
